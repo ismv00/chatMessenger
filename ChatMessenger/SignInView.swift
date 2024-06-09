7e3b98670cc90a9a07a4e4cc49dbb3e976970337
ismv00 <euigoorr@MacBook-Pro-de-Igor.local>
2024-06-09 13:01:58 -0400

    feat:adc comentários no código

--- ChatMessenger/SignInView.swift
+++ ChatMessenger/SignInView.swift
@@ -9,27 +9,27 @@ import SwiftUI
 
 struct SignInView: View {
     
-    @StateObject var viewModel = SignInViewModel()
+    @StateObject var viewModel = SignInViewModel() // Instanciando a ViewModel que contem o modelo de dados e salvando na variável viewmodel
     
     var body: some View {
         NavigationView {
             VStack {
-                Image("chat_logo")
+                Image("chat_logo") // Redimensionar a imagem
                     .resizable()
                     .scaledToFit()
                     .padding()
                 
                 TextField("Entre com seu e-mail", text: $viewModel.email)
-                    .autocapitalization(.none)
-                    .disableAutocorrection(false)
+                    .autocapitalization(.none) // Sem letra maiuscula no começo
+                    .disableAutocorrection(false) // Desabilita o corretor
                     .padding()
                     .background(Color.white)
-                    .cornerRadius(24.0)
-                    .overlay(RoundedRectangle(cornerRadius: 24.0)
+                    .cornerRadius(24.0) // bordas arredondadas
+                    .overlay(RoundedRectangle(cornerRadius: 24.0) // Sombra ao redor do texfiel
                         .stroke(Color(uiColor: .separator), style: StrokeStyle(lineWidth: 1.0)))
-                    .padding(.bottom, 20)
+                    .padding(.bottom, 20) // Padding só na parte de baixo para o próximo elemento
                 
-                SecureField("Entre com sua senha", text: $viewModel.password)
+                SecureField("Entre com sua senha", text: $viewModel.password) // campo de senha
                     .autocapitalization(.none)
                     .disableAutocorrection(false)
                     .padding()
@@ -39,16 +39,16 @@ struct SignInView: View {
                         .stroke(Color(uiColor: .separator), style: StrokeStyle(lineWidth: 1.0)))
                     .padding(.bottom, 30)
                 
-                if viewModel.isLoading {
+                if viewModel.isLoading { // Se a var isLoading for true, carrega a progress view
                     ProgressView()
                         .padding()
                 }
                 
                 Button {
-                    viewModel.signIn()
+                    viewModel.signIn() // Chama a função de login da viewmodel
                 } label: {
                     Text("Entrar")
-                        .frame(maxWidth: .infinity)
+                        .frame(maxWidth: .infinity) // estica o elemento para pegar a pagina toda. Apenas na margin
                         .padding()
                         .background(Color("GreenColor"))
                         .foregroundColor(Color.white)
@@ -59,16 +59,13 @@ struct SignInView: View {
                 
                 }
                 
-                Divider()
+                Divider() // Linha que divide os elementos
                     .padding()
                 
-                NavigationLink(destination: SignUpView()) {
+                NavigationLink(destination: SignUpView()) { // Navegação para a próxima página
                     Text("Não tem uma conta? Clique aqui.")
                         .foregroundColor(.black)
                 }
-                
-                
-                
                 
             }
             .frame(maxWidth: .infinity ,maxHeight: .infinity)
--- ChatMessenger/SignInViewModel.swift
+++ ChatMessenger/SignInViewModel.swift
@@ -21,8 +21,12 @@ class SignInViewModel: ObservableObject {
     
     func signIn() {
         print("email: \(email)", "senha: \(password)")
-        self.isLoading = true
+        self.isLoading = true // altera a variavel para true para ser apresentada em tela
         
+        // Chama o metodo de autenticação por e-mail e senha do Firebase, pegando o e-mail e senha enviados.
+        // Se existir um usuário e senha corretos, a variavel isLoading vira false e o uid do usuário é apresentado
+        // Se não existir um usuário, o formuário de erro é mostrado (formInvalid) e salvamos a descriçào do erro na variável alertText
+        // o isLoading vira false e eu retorno esse erro para ser mostrado em tela.
         Auth.auth().signIn(withEmail: email, password: password) {
             result, err in
             guard let user = result?.user, err == nil else {
