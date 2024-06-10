1af9c8592b11f3e5f7153651d97bfe28705ac272
ismv00 <euigoorr@MacBook-Pro-de-Igor.local>
2024-06-09 21:59:37 -0400

    feat:envia a imagem para o bucket do firebase

--- ChatMessenger/SignUpViewModel.swift
+++ ChatMessenger/SignUpViewModel.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseStorage
 
 class SignUpViewModel : ObservableObject {
     var name = ""
@@ -21,8 +22,17 @@ class SignUpViewModel : ObservableObject {
     @Published var isLoading = false
     
     func signUp() {
-        isLoading = true
+
         print("nome: \(name)", "email: \(email)", "senha: \(password)")
+        
+        //Verifica se o tamanho da imagem é menor que zero, se for pede para selecionar uma foto.
+        if (image.size.width <= 0 ) {
+            formInvalid = true
+            alertText = "Selecione uma foto."
+            return
+        }
+        
+        isLoading = true
         
         Auth.auth().createUser(withEmail: email, password: password) {
             result, err in
@@ -35,6 +45,29 @@ class SignUpViewModel : ObservableObject {
             }
             self.isLoading = false
             print("Usuário criado \(user.uid)")
+            
+            // Chama a função de fazer o upload
+            self.uploadPhoto()
+        }
+    }
+    
+    //Função para fazer upload da foto de perfil.
+    private func uploadPhoto() {
+        
+        let filename = UUID().uuidString // Nome aleatório para a imagem
+        guard let data = image.jpegData(compressionQuality: 0.2) else { return } // Guarda os dados e a taxa de compressão da foto.
+        
+        let newMetadata = StorageMetadata() // Pega os metadados da imagem lá do storage
+        newMetadata.contentType = "image/jpeg"
+        
+        let ref = Storage.storage().reference(withPath: "/images/\(filename).jpg") // Salva a referência do caminho da imagem.
+        
+        // Envia os dados.
+        ref.putData(data, metadata: newMetadata) {metadata, err in
+            ref.downloadURL {url, error in
+                self.isLoading = false
+                print("Foto criada \(url)")
+            }
         }
     }
 }
