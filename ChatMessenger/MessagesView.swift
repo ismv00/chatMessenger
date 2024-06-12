ffa9d62fb38a3f3a76f442e0b2889504a0777dba
ismv00 <euigoorr@MacBook-Pro-de-Igor.local>
2024-06-11 23:04:24 -0400

    feat:create a navigation contacts screen

--- ChatMessenger/MessagesView.swift
+++ ChatMessenger/MessagesView.swift
@@ -11,10 +11,22 @@ struct MessagesView: View {
     @StateObject var viewModel = MessagesViewModel()
     
     var body: some View {
-        Button {
-            viewModel.logout()
-        } label: {
-            Text("Logout")
+        NavigationView {
+            VStack {
+                Text("Ola Mundo")
+            }
+            .toolbar{
+                ToolbarItem(id: "contacts",
+                            placement: ToolbarItemPlacement.navigationBarTrailing, showsByDefault: true) {
+                    NavigationLink("Contatos", destination: Text("Tela de contatos falsa"))
+                }
+                ToolbarItem(id: "logout",
+                            placement: ToolbarItemPlacement.navigationBarTrailing, showsByDefault: true) {
+                    Button("Logout") {
+                        viewModel.logout()
+                    }
+                }
+            }
         }
     }
 }
